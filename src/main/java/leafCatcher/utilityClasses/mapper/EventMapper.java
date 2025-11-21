ed636200b5dda16f246bd5d95b6ea4204c901bc0
Igor_blaz <koloda5600@gmail.com>
2025-11-21 14:46:31 +0300

    o

--- src/main/java/leafCatcher/utilityClasses/mapper/EventMapper.java
+++ src/main/java/leafCatcher/utilityClasses/mapper/EventMapper.java
@@ -14,7 +14,7 @@ public class EventMapper {
         child.setTelegramId(1L);
         child.setUpdateId(0);
         child.setDescription(description);
-        child.setShortName(buttonName);
+        child.setShortName("▶️ " + buttonName);
         child.setIsRoot(false);
         child.setIsEnd(isEnd);
         child.setIsChangeable(true);
@@ -46,7 +46,7 @@ public class EventMapper {
         root.setTelegramId(0L);
         root.setUpdateId(0);
         root.setDescription(description);
-        root.setShortName(buttonName);
+        root.setShortName("❤️ " + buttonName);
         root.setIsRoot(true);
         root.setIsEnd(false);
         root.setIsChangeable(true);
